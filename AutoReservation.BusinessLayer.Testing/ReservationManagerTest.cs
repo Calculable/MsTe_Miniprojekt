3a336ac9dac454db083b119ba49edccec1d7ddda
Huber Jan (hurj) <hurj@zhaw.ch>
2019-11-23 16:54:56 +0100

    Implemented Reservation-Manager (business logic) and added some tests

---
+++ AutoReservation.BusinessLayer.Testing/ReservationManagerTest.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoReservation.BusinessLayer.Exceptions;
+using AutoReservation.Dal.Entities;
+using AutoReservation.TestEnvironment;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Xunit;
+
+namespace AutoReservation.BusinessLayer.Testing
+{
+    public class ReservationManagerTest
+        : TestBase
+    {
+        private readonly ReservationManager _target;
+
+        public ReservationManagerTest()
+        {
+            _target = new ReservationManager();
+        }
+
+
+
+        [Fact]
+        public async Task GetAllReservationenTest()
+        {
+            List<Reservation> reservations = await _target.GetAll();
+            Assert.Equal(4, reservations.Count);
+
+        }
+
+        [Fact]
+        public async Task InsertReservationTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+
+            int newId = _target.insert(reservation);
+
+            List<Reservation> reservations = await _target.GetAll();
+            Assert.Equal(5, reservations.Count);
+        }
+
+        [Fact]
+        public void GetReservationForKeyTest()
+        {
+            Reservation reservationToInsert = createNewExampleReservation();
+            int newId = _target.insert(reservationToInsert);
+
+            Reservation inserted = _target.GetForKey(newId);
+            Assert.Equal(reservationToInsert.Bis, inserted.Bis);
+            Assert.Equal(reservationToInsert.Von, inserted.Von);
+
+            //check that kunde und auto are loaded too
+            Assert.Equal(reservationToInsert.Kunde.Name, inserted.Kunde.Name);
+            Assert.Equal(reservationToInsert.Auto.Marke, inserted.Auto.Marke);
+
+        }
+
+        [Fact]
+        public void GetReservationForNonExistingKeyThrowsExceptionTest()
+        {
+            var ex = Assert.Throws<InvalidOperationException>(() => _target.GetForKey(999));
+            Assert.Equal("Sequence contains no elements", ex.Message);
+        }
+
+        [Fact]
+        public void InsertAlreadyExistingReservationThrowsExceptionTest()
+        {
+            Reservation toInsert = createNewExampleReservation();
+            int newId = _target.insert(toInsert);
+
+
+            Reservation inserted = _target.GetForKey(newId);
+
+            Assert.Throws<DbUpdateException>(() => _target.insert(inserted));
+
+
+        }
+
+        [Fact]
+        public async Task DeleteReservationTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+            int newId = _target.insert(reservation);
+
+            List<Reservation> reservationen = await _target.GetAll();
+            Assert.Equal(5, reservationen.Count);
+
+            Reservation inserted = _target.GetForKey(newId);
+            _target.delete(inserted);
+
+            reservationen = await _target.GetAll();
+            Assert.Equal(4, reservationen.Count);
+        }
+
+        [Fact]
+        public void DeleteNotInsertedReservationThrowsExceptionTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+
+            Assert.Throws<InvalidOperationException>(() => _target.delete(reservation));
+
+        }
+
+        [Fact]
+        public void DeleteUpdatedReservationThrowsExceptionTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+            int newId = _target.insert(reservation);
+
+
+            Reservation inserted1 = _target.GetForKey(newId);
+            Reservation inserted2 = _target.GetForKey(newId);
+            inserted2.Bis = new DateTime(2021, 03, 10);
+            _target.update(inserted2);
+
+            var ex = Assert.Throws<OptimisticConcurrencyException<PropertyValues>>(() => _target.delete(inserted1));
+        }
+
+        [Fact]
+        public void UpdateReservationTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+            int newId = _target.insert(reservation);
+
+            reservation.Bis = new DateTime(2021, 03, 10);
+            _target.update(reservation);
+
+            Reservation inserted = _target.GetForKey(newId);
+
+            Assert.Equal(new DateTime(2021, 03, 10), inserted.Bis);
+        }
+
+        [Fact]
+        public void UpdateKundeViaReservationTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+            int newId = _target.insert(reservation);
+
+            reservation.Kunde.Name = "changed";
+            _target.update(reservation);
+
+            Reservation inserted = _target.GetForKey(newId);
+
+            Assert.Equal("changed", inserted.Kunde.Name);
+        }
+
+        [Fact]
+        public void UpdateChangedReservationThrowsExceptionTest()
+        {
+            Reservation reservation = createNewExampleReservation();
+            int newId = _target.insert(reservation);
+
+
+            Reservation inserted1 = _target.GetForKey(newId);
+            Reservation inserted2 = _target.GetForKey(newId);
+
+            reservation.Bis = new DateTime(2021, 03, 10);
+            _target.update(inserted1);
+
+            reservation.Bis = new DateTime(2021, 04, 10);
+
+            var ex = Assert.Throws<OptimisticConcurrencyException<PropertyValues>>(() => _target.update(inserted2));
+        }
+
+        private Reservation createNewExampleReservation()
+        {
+            Auto auto = new LuxusklasseAuto { Marke = "Example Marke", Tagestarif = 100, Basistarif = 200 };
+            Kunde kunde = new Kunde { Name = "Müller", Vorname = "Judith", Geburtsdatum = new DateTime(1980, 02, 13) };
+            return new Reservation { Auto = auto, Kunde = kunde, Von = new DateTime(2020, 01, 10), Bis = new DateTime(2020, 01, 20) };
+        }
+
+
+
+        /*            Auto[] autos = {
+                new StandardAuto {Marke = "Fiat Punto", Tagestarif = 50},
+                new MittelklasseAuto {Marke = "VW Golf", Tagestarif = 120},
+                new LuxusklasseAuto {Marke = "Audi S6", Tagestarif = 180, Basistarif = 50},
+                new StandardAuto {Marke = "Fiat 500", Tagestarif = 75},
+            };
+
+            Kunde[] kunden = {
+                new Kunde {Name = "Nass", Vorname = "Anna", Geburtsdatum = new DateTime(1981, 05, 05)},
+                new Kunde {Name = "Beil", Vorname = "Timo", Geburtsdatum = new DateTime(1980, 09, 09)},
+                new Kunde {Name = "Pfahl", Vorname = "Martha", Geburtsdatum = new DateTime(1990, 07, 03)},
+                new Kunde {Name = "Zufall", Vorname = "Rainer", Geburtsdatum = new DateTime(1954, 11, 11)},
+            };
+
+            int year = DateTime.Now.Year + 1;
+            Reservation[] reservationen = {
+                new Reservation {Auto = autos[0], Kunde = kunden[0], Von = new DateTime(year, 01, 10), Bis = new DateTime(year, 01, 20)},
+                new Reservation {Auto = autos[1], Kunde = kunden[1], Von = new DateTime(year, 01, 10), Bis = new DateTime(year, 01, 20)},
+                new Reservation {Auto = autos[2], Kunde = kunden[2], Von = new DateTime(year, 01, 10), Bis = new DateTime(year, 01, 20)},
+                new Reservation {Auto = autos[1], Kunde = kunden[0], Von = new DateTime(year, 05, 19), Bis = new DateTime(year, 06, 19)},
+            };*/
+    }
+}
--- AutoReservation.BusinessLayer/ReservationManager.cs
+++ AutoReservation.BusinessLayer/ReservationManager.cs
@@ -1,10 +1,130 @@
 ﻿using System;
 using System.Linq;
+using AutoReservation.BusinessLayer.Exceptions;
+using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace AutoReservation.BusinessLayer
 {
     public class ReservationManager
         : ManagerBase
     {
+        public async Task<List<Reservation>> GetAll()
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return await context
+                    .Reservationen
+                    .Include(reservation => reservation.Kunde)
+                    .Include(reservation => reservation.Auto)
+                    .ToListAsync();
+            }
+        }
+
+        public Reservation GetForKey(int reservationsNummer)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return context.Reservationen
+                    .Include(reservation => reservation.Kunde)
+                    .Include(reservation => reservation.Auto)
+                    .Single(reservation => reservation.ReservationsNr == reservationsNummer);
+            }
+        }
+
+        public int insert(Reservation reservation)
+        {
+            assertDateRangeIsValid(reservation);
+            assertCarIsAvaliable(reservation);
+
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Reservationen.Add(reservation);
+
+                autoReservationContext.SaveChanges();
+                return reservation.ReservationsNr;
+            }
+        }
+
+        public int update(Reservation reservation)
+        {
+            assertDateRangeIsValid(reservation);
+            assertCarIsAvaliable(reservation);
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Reservationen.Update(reservation);
+
+                try
+                {
+                    autoReservationContext.SaveChanges();
+                    return reservation.ReservationsNr;
+                }
+                catch (DbUpdateConcurrencyException concurrencyException)
+                {
+                    var originalValues = concurrencyException.Entries.First().OriginalValues;
+                    throw new OptimisticConcurrencyException<PropertyValues>("Can not update reservation because of a concurrency exception. Maybe the the reservation was updated in the meantime?", originalValues);
+                }
+            }
+        }
+
+        public void delete(Reservation reservation)
+        {
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Reservationen.Remove(reservation);
+                try
+                {
+                    autoReservationContext.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException concurrencyException)
+                {
+                    var originalValues = concurrencyException.Entries.First().OriginalValues;
+                    throw new OptimisticConcurrencyException<PropertyValues>("Can not delete reservation because of a concurrency exception. Maybe the the reservation was updated in the meantime?", originalValues);
+                }
+            }
+        }
+
+        private void assertDateRangeIsValid(Reservation reservation)
+        {
+            DateTime von = reservation.Von;
+            DateTime bis = reservation.Bis;
+
+            if (von >= bis)
+            {
+                throw new InvalidDateRangeException("Das von-Datum darf nicht grösser sein als das bis-Datum", von, bis);
+            }
+
+            if (von.AddDays(1) > bis)
+            {
+                throw new InvalidDateRangeException("Zwischen von-Datum uns bis-Datum müssen mindestens 24 Stunden liegen", von, bis);
+            }
+        }
+
+        private void assertCarIsAvaliable(Reservation currentReservation)
+        {
+
+            
+                using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+                {
+                   int amountOfOverlappingReservations = autoReservationContext
+                   .Reservationen
+                   .Include(reservation => reservation.Auto)
+                   .Where(reservation => reservation.AutoId == currentReservation.AutoId)
+                   .Where(reservation => reservation.ReservationsNr != currentReservation.ReservationsNr)
+                   .Where(reservation => (reservation.Von > currentReservation.Von && reservation.Von < currentReservation.Bis) || (reservation.Bis > currentReservation.Von && reservation.Bis < currentReservation.Bis))
+                   .Count();
+
+                    if (amountOfOverlappingReservations > 0)
+                    {
+                        throw new AutoUnavailableException("Die Reservierung ist ungültig, da das reservierte Fahrzeug in diesem Zeitrahmen bereits reserviert ist."); 
+                    }
+            }
+            
+           
+        }
     }
 }
