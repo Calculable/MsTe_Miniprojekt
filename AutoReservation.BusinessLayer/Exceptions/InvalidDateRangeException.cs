668b4596b84fb8c9ab8204fa8e4cf73e8e672b88
Huber Jan (hurj) <hurj@zhaw.ch>
2019-11-23 16:54:27 +0100

    Add Exception-Classes

---
+++ AutoReservation.BusinessLayer/Exceptions/InvalidDateRangeException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AutoReservation.BusinessLayer.Exceptions
+{
+    public class InvalidDateRangeException 
+        : Exception
+    {
+        public InvalidDateRangeException(string message, DateTime firstDate, DateTime secondDate) : base(message)
+        {
+            this.firstDate = firstDate;
+            this.secondDate = secondDate;
+              
+        }
+
+        public DateTime firstDate { get; set; }
+        public DateTime secondDate { get; set; }
+
+    }
+}
