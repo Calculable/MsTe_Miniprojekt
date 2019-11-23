e1d4903a80f91cab2ed3ba3bde9e48181ae47230
Huber Jan (hurj) <hurj@zhaw.ch>
2019-11-23 10:43:34 +0100

    Implement Auto Manager (Business Logic)

--- AutoReservation.BusinessLayer/AutoManager.cs
+++ AutoReservation.BusinessLayer/AutoManager.cs
@@ -1,6 +1,8 @@
-﻿using AutoReservation.Dal;
+﻿using AutoReservation.BusinessLayer.Exceptions;
+using AutoReservation.Dal;
 using AutoReservation.Dal.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +15,63 @@ namespace AutoReservation.BusinessLayer
     {
         public async Task<List<Auto>> GetAll()
         {
-            using AutoReservationContext context = new AutoReservationContext();
-           return await context.Autos.ToListAsync();
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return await context.Autos.ToListAsync();
+            }
         }
+
+        public Auto GetForKey(int autoKey)
+        {
+            using (AutoReservationContext context = new AutoReservationContext())
+            {
+                return context.Autos.Single(auto => auto.Id == autoKey);
+            }
+        }
+
+        public int insert(Auto auto)
+        {
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Entry(auto).State = EntityState.Added;
+                autoReservationContext.SaveChanges();
+                return auto.Id;
+            }
+        }
+
+        public int update(Auto auto)
+        {
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Entry(auto).State = EntityState.Modified;
+
+                try
+                {
+                    autoReservationContext.SaveChanges();
+                    return auto.Id;
+                } catch (DbUpdateConcurrencyException concurrencyException)
+                {
+                    throw new OptimisticConcurrencyException<PropertyValues>("Can not update because of a concurrency exception. Maybe the the object was updated in the meantime?", concurrencyException.Entries.First().GetDatabaseValues());
+                }
+            }
+        }
+
+        public void delete(Auto auto)
+        {
+            using (AutoReservationContext autoReservationContext = new AutoReservationContext())
+            {
+                autoReservationContext.Entry(auto).State = EntityState.Deleted;
+
+                try
+                {
+                    autoReservationContext.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException concurrencyException)
+                {
+                    throw new OptimisticConcurrencyException<PropertyValues>("Can not delete because of a concurrency exception. Maybe the the object was updated in the meantime?", concurrencyException.Entries.First().GetDatabaseValues());
+                }
+            }
+        }
+
     }
 }
