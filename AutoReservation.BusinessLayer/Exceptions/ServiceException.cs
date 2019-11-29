d9fcd27fd3207b4074d69047653ca3245b0c9796
Jan Huber <hurj@zhaw.ch>
2019-11-29 15:48:30 +0100

    Added Exception-Handling in Service-Layer

---
+++ AutoReservation.BusinessLayer/Exceptions/ServiceException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AutoReservation.BusinessLayer.Exceptions
+{
+    public class ServiceException<T>
+        : Exception
+    {
+        public ServiceException(OptimisticConcurrencyException<T> innerException) : base("Bei der Kommunikation mit der Datenbank ist ein Fehler aufgetreten. Bitte versuchen Sie es nochmals", innerException) { }
+       
+    }
+}
--- AutoReservation.Service.Grpc/Services/AutoService.cs
+++ AutoReservation.Service.Grpc/Services/AutoService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoReservation.BusinessLayer;
+using AutoReservation.BusinessLayer.Exceptions;
 using AutoReservation.Dal.Entities;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
@@ -21,9 +22,17 @@ namespace AutoReservation.Service.Grpc.Services
         {
             Auto autoEntity = DtoConverter.ConvertToEntity(autoDTO);
             AutoManager autoManager = new AutoManager();
-            autoManager.delete(autoEntity);
-            return Task.FromResult(new Empty());
-        }
+
+            try
+            {
+                autoManager.delete(autoEntity);
+                return Task.FromResult(new Empty());
+            } catch (OptimisticConcurrencyException<Auto> exception)
+            {
+                throw new ServiceException<Auto>(exception);
+            }
+            
+        } 
 
         public override Task<Empty> InsertAuto(AutoDTO autoDTO, ServerCallContext context)
         {
@@ -37,8 +46,7 @@ namespace AutoReservation.Service.Grpc.Services
         {
             AutoManager autoManager = new AutoManager();
             Task<List<Auto>> allAuto = autoManager.GetAll();
-            return DtoConverter.ConvertToDtos(allAuto);
-            
+            return DtoConverter.ConvertToDtos(allAuto);   
         }
 
         public override Task<AutoDTO> ReadAutoForId(AutoIdentifier autoDTOIdentifier, ServerCallContext context)
@@ -46,19 +54,22 @@ namespace AutoReservation.Service.Grpc.Services
             AutoManager autoManager = new AutoManager();
             Auto autoEntity = autoManager.GetForKey(autoDTOIdentifier.Id);
             return Task.FromResult(DtoConverter.ConvertToDto(autoEntity));
-
         }
 
         public override Task<Empty> UpdateAuto(AutoDTO autoDTO, ServerCallContext context)
         {
             Auto autoEntity = DtoConverter.ConvertToEntity(autoDTO);
             AutoManager autoManager = new AutoManager();
-            autoManager.update(autoEntity);
+            try
+            {
+                autoManager.update(autoEntity);
+            }
+            catch (OptimisticConcurrencyException<Auto> exception)
+            {
+                throw new ServiceException<Auto>(exception);
+            }
+
             return Task.FromResult(new Empty());
         }
-
-
     }
-
-
 }
